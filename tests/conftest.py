8a088550883a87daeef14b3d0cdbbad6c217dabc
HAMIDREZA <soltanshahhamidreza@gmail.com>
2026-04-13 23:11:52 +0200

    Refactor environment variable setup in conftest.py
    Updated environment variable assignments to use os.environ[] syntax and changed LOG_LEVEL to WARNING.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,14 +7,14 @@ import pytest
 from httpx import ASGITransport, AsyncClient
 
 
-os.environ.setdefault("APP_ENV", "development")
-os.environ.setdefault("APP_DEBUG", "true")
-os.environ.setdefault("BOT_TOKEN", "test_bot_token")
-os.environ.setdefault("MONGO_URI", "mongodb://127.0.0.1:27017")
-os.environ.setdefault("MONGO_DB_NAME", "time_manager_pro_test")
-os.environ.setdefault("LOG_LEVEL", "INFO")
-os.environ.setdefault("TELEGRAM_INITDATA_MAX_AGE", "900")
-os.environ.setdefault("TELEGRAM_INITDATA_FUTURE_SKEW", "60")
+os.environ["APP_ENV"] = "development"
+os.environ["APP_DEBUG"] = "true"
+os.environ["BOT_TOKEN"] = os.environ.get("BOT_TOKEN", "test_bot_token")
+os.environ["MONGO_URI"] = os.environ.get("MONGO_URI", "mongodb://127.0.0.1:27017")
+os.environ["MONGO_DB_NAME"] = "time_manager_pro_test"  # همیشه override
+os.environ["LOG_LEVEL"] = "WARNING"
+os.environ["TELEGRAM_INITDATA_MAX_AGE"] = "900"
+os.environ["TELEGRAM_INITDATA_FUTURE_SKEW"] = "60"
 
 
 @pytest.fixture(scope="session")
